f76fbfef9d5546b21956c788211891fe2a7bcd41
Arun <ararun0711@gmail.com>
2025-08-14 11:26:50 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+function ski(){
+    location.href="https://avriuno.github.io/skill-fed-id/"
+}
+function con(){
+    location.href="https://avriuno.github.io/contact-fed-id/"
+}
+
+function abo(){
+    location.href="https://avriuno.github.io/about-fed-id/"
+}
+
+function edu(){
+    location.href="https://avriuno.github.io/educate-id/"
+}
+
+function pro(){
+    location.href="https://avriuno.github.io/project-id/"
+}
